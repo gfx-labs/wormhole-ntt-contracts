8985bebf1a727c2f3b0807452fc993a526276738
Maximiliano Molina <hpmaxi@gmail.com>
2024-11-07 12:43:33 -0300

    fix create3 on factory

--- script/NttFactory.s.sol
+++ script/NttFactory.s.sol
@@ -2,34 +2,30 @@
 pragma solidity ^0.8.13;
 
 import {Script, console2} from "forge-std/Script.sol";
-import {CREATE3} from "solmate/utils/CREATE3.sol";
 import {NttFactory} from "../src/NttFactory.sol";
+import {CREATE3Factory} from "lib/create3-factory/src/CREATE3Factory.sol";
 
-contract NttFactoryDeploy is Script {
+contract NttFactoryDeploy is Script, CREATE3Factory {
     function setUp() public {}
 
     function run() public payable {
         bytes32 VERSION = keccak256(abi.encodePacked(vm.envString("VERSION")));
 
-        // Each deployer with its own namespace
-        bytes32 FACTORY_SALT = keccak256(abi.encodePacked(msg.sender, VERSION));
-
         // Hardcoded EnvParams from environment variables
-        address wormholeCoreBridge = vm.envAddress("WORMHOLE_CORE_BRIDGE");
-        address wormholeRelayerAddr = vm.envAddress("WORMHOLE_RELAYER_ADDR");
-        address specialRelayerAddr = vm.envAddress("SPECIAL_RELAYER_ADDR");
+        // address wormholeCoreBridge = vm.envAddress("WORMHOLE_CORE_BRIDGE");
+        // address wormholeRelayerAddr = vm.envAddress("WORMHOLE_RELAYER_ADDR");
+        // address specialRelayerAddr = vm.envAddress("SPECIAL_RELAYER_ADDR");
 
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address deployer = vm.addr(deployerPrivateKey);
+        // Each deployer with its own namespace
+        bytes32 FACTORY_SALT = keccak256(abi.encodePacked(deployer, VERSION));
 
         console2.log("Deploying factory with private key:", deployerPrivateKey);
         vm.startBroadcast(deployerPrivateKey);
 
         NttFactory factory = NttFactory(
-            CREATE3.deploy(
-                FACTORY_SALT,
-                abi.encodePacked(type(NttFactory).creationCode, abi.encode(msg.sender, VERSION)),
-                msg.value
-            )
+            this.deploy(FACTORY_SALT, abi.encodePacked(type(NttFactory).creationCode, abi.encode(deployer, VERSION)))
         );
 
         vm.stopBroadcast();
