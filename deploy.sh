fb3e620a2418841b44294701f32c7dc8d56bcdf7
Maximiliano Molina <hpmaxi@gmail.com>
2024-12-20 14:06:57 -0300

    script to deploy factories

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-# Load environment variables
 source .env
 
 # Default configuration
@@ -9,13 +7,23 @@ VERBOSITY="-vvvv"
 SENDER="0x7EA57D754a845808c1f640271A46df98F8e75894"
 SCRIPT_PATH="script/NttFactory.s.sol:NttFactoryDeploy"
 
-for network in sepolia arbitrum_sepolia base_sepolia optimism_sepolia; do
-    echo "\nDeploying to $network..."
-    forge script $SCRIPT_PATH \
-        --broadcast \
-        --sender $SENDER \
-        -vv \
-        --via-ir \
-        --verify \
-        --rpc-url $network
-done
+# Read CSV and deploy
+while IFS=, read -r network addr1 addr2 addr3
+do
+    # Skip header
+    if [ "$network" != "network" ]; then
+        echo "Deploying to $network"
+
+        export WORMHOLE_CORE_BRIDGE=$addr1
+        export WORMHOLE_RELAYER=$addr2
+        export SPECIAL_RELAYER=$addr3
+        
+        forge script $SCRIPT_PATH \
+            --broadcast \
+            --sender $SENDER \
+            -vv \
+            --via-ir \
+            --verify \
+            --rpc-url $network 
+    fi
+done < addresses.csv
